deccc9d551833089d3684a476bcd0e03594a5a33
Sergey Kondratovec <sergey@spheremall.com>
2017-11-08 17:58:46 +0200

    Add property array for entities

--- src/Entities/Entity.php
+++ src/Entities/Entity.php
@@ -15,6 +15,11 @@ namespace SphereMall\MS\Entities;
  */
 class Entity
 {
+    #region [Properties]
+    protected $properties = [];
+    #endregion
+
+    #region [Constructor]
     /**
      * Entity constructor.
      * @param array $data
@@ -28,14 +33,35 @@ class Entity
         foreach ($data as $optionKey => $optionValue) {
             if (property_exists($this, $optionKey)) {
                 $this->{$optionKey} = $optionValue;
+            } else {
+                $this->properties[$optionKey] = $optionValue;
             }
         }
 
         return $this;
     }
+    #endregion
 
+    #region [Public methods]
+    /**
+     * Get value by name from property of class or $properties if value is not exist in class
+     * @see $properties
+     * @param $key
+     * @return bool
+     */
+    public function getProperty($key)
+    {
+        if(isset($this->properties[$key]))
+            return $this->properties[$key];
+
+        return null;
+    }
+    /**
+     * @return array
+     */
     public function asArray()
     {
         return get_object_vars($this);
     }
+    #endregion
 }
