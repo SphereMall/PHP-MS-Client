e5a8b2617d05568dd9d56369e1e64a21dd414f23
Sergey Kondratovec <sergey@spheremall.com>
2017-12-20 14:28:36 +0200

    Entity test

---
+++ tests/Lib/Entities/EntityTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PHPStorm.
+ * User: Serhii Kondratovec
+ * Email: sergey@spheremall.com
+ * Date: 12/18/2017
+ * Time: 2:52 PM
+ */
+
+
+namespace SphereMall\MS\Tests\Lib\Entities;
+
+
+class EntityTest
+{
+
+}
