cc745b6bf2ab2b5068a7afde79717ad39638d4b7
s.yanchevsky <s.yanshevsky@spheremall.com>
2018-03-01 10:40:54 +0200

    MIC-175 - move vars to class level

--- tests/Lib/Helpers/HttpHelperTest.php
+++ tests/Lib/Helpers/HttpHelperTest.php
@@ -16,43 +16,52 @@ use SphereMall\MS\Lib\Helpers\HttpHelper as Helper;
 /**
  * Class HttpHelper
  * @package SphereMall\MS\Tests\Lib\Helpers
+ *
+ * @property string $url
+ * @property string $urlDefault
  */
 class HttpHelperTest extends SetUpResourceTest
 {
 
+    protected $url        = 'example.com/v1/test';
+    protected $urlDefault = 'example.com:80/v1/test\'';
+
+    /**
+     *
+     */
     public function testAddPort()
     {
-        $url        = 'example.com/v1/test';
-        $urlDefault = 'example.com:80/v1/test';
+        $this->assertEquals($this->urlDefault, Helper::addPort($this->url, 80));
 
-        $this->assertEquals($urlDefault, Helper::addPort($url, 80));
+        $this->url        = 'example.com';
+        $this->urlDefault = 'example.com:80';
 
-        $url        = 'example.com';
-        $urlDefault = 'example.com:80';
-
-        $this->assertEquals($urlDefault, Helper::addPort($url, 80));
+        $this->assertEquals( $this->urlDefault, Helper::addPort($this->url, 80));
     }
 
+    /**
+     *
+     */
     public function testSetHttPortToUrl()
     {
-        $url        = 'http://example.com/v1/test';
-        $urlDefault = 'http://example.com:443/v1/test';
+        $this->url        = 'http://example.com/v1/test';
+        $this->urlDefault = 'http://example.com:443/v1/test';
 
-        $this->assertEquals($urlDefault, Helper::setHttPortToUrl($url, true));
+        $this->assertEquals( $this->urlDefault, Helper::setHttPortToUrl( $this->url, true));
 
         $urlDefault = 'http://example.com:80/v1/test';
 
-        $this->assertEquals($urlDefault, Helper::setHttPortToUrl($url, false));
+        $this->assertEquals($urlDefault, Helper::setHttPortToUrl($this->url, false));
 
-        $url        = 'http://example.com:9020/v1/test';
-        $urlDefault = 'http://example.com:9020/v1/test';
+        $this->url        = 'http://example.com:9020/v1/test';
+        $this->urlDefault = 'http://example.com:9020/v1/test';
 
-        $this->assertEquals($urlDefault, Helper::setHttPortToUrl($url, false));
-        $this->assertEquals($urlDefault, Helper::setHttPortToUrl($url, true));
+        $this->assertEquals($urlDefault, Helper::setHttPortToUrl($this->url, false));
+        $this->assertEquals($urlDefault, Helper::setHttPortToUrl($this->url, true));
 
-        $url        = 'https://example.com/v1/test';
-        $urlDefault = 'https://example.com:443/v1/test';
+        $this->url        = 'https://example.com/v1/test';
+        $this->urlDefault = 'https://example.com:443/v1/test';
 
-        $this->assertEquals($urlDefault, Helper::setHttPortToUrl($url));
+        $this->assertEquals($this->urlDefault, Helper::setHttPortToUrl($this->url));
     }
 }
