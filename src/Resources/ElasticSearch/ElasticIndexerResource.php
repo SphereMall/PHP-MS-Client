8e15075a38ef236d4cd0cd9160acb712071636b6
Oleksandr Rokytskyi <o.rokytskyi@gmail.com>
2018-03-30 10:37:41 +0300

    ElasticeIndexerResource created

---
+++ src/Resources/ElasticSearch/ElasticIndexerResource.php
@@ -0,0 +1,103 @@
+<?php
+
+use SphereMall\MS\Exceptions\MethodNotFoundException;
+use SphereMall\MS\Resources\Resource;
+
+/**
+ * Created by PhpStorm.
+ * User: Dmitriy
+ * Date: 30.03.2018
+ * Time: 10:00
+ */
+
+class ElasticIndexerResource extends Resource{
+
+    /**
+     * @return string
+     */
+    public function getURI()
+    {
+        return 'elasticsearch';
+    }
+
+    /**
+     * @return array
+     * @throws Exception
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function runIndex(){
+        $response = $this->handler->handle('GET', null, 'runindex');
+        return $this->make($response, false, new ElasticIndexerResponseMaker());
+
+    }
+
+    /**
+     * @return array
+     * @throws Exception
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function reindex(){
+        $response = $this->handler->handle('GET', null, 'reindex');
+        return $this->make($response, false, new ElasticIndexerResponseMaker());
+    }
+
+    /**
+     * @return array
+     * @throws Exception
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function deleteIndex()
+    {
+        $response = $this->handler->handle('DELETE', null, 'delete');
+        return $this->make($response, false, new ElasticIndexerResponseMaker());
+    }
+
+    /**
+     * @return array|void
+     * @throws MethodNotFoundException
+     */
+    public function all()
+    {
+        throw new MethodNotFoundException("Method all() can not be use with Elasticsearch");
+    }
+
+    /**
+     * @return int|void
+     * @throws MethodNotFoundException
+     */
+    public function count()
+    {
+        throw new MethodNotFoundException("Method count() can not be use with Elasticsearch");
+    }
+
+    /**
+     * @param int $id
+     * @return array|\SphereMall\MS\Entities\Entity|void
+     * @throws MethodNotFoundException
+     */
+    public function get(int $id)
+    {
+        throw new MethodNotFoundException("Method get() can not be use with Elasticsearch");
+    }
+
+    /**
+     * @param $id
+     * @param $data
+     * @return \SphereMall\MS\Entities\Entity|void
+     * @throws MethodNotFoundException
+     */
+    public function update($id, $data)
+    {
+        throw new MethodNotFoundException("Method update() can not be use with Elasticsearch");
+    }
+
+    /**
+     * @param $data
+     * @return \SphereMall\MS\Entities\Entity|void
+     * @throws MethodNotFoundException
+     */
+    public function create($data)
+    {
+        throw new MethodNotFoundException("Method create() can not be use with Elasticsearch");
+    }
+}
