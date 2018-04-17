909102aacc1308fdd92eca1343aa45d035b271ed
Viktor Matushevskyi <v.matushevskyi@spheremall.com>
2018-04-17 14:53:31 +0300

    Add unit of measure

---
+++ src/Entities/UnitOfMeasure.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: [Viktor Matushevskyi](mailto:v.matushevskyi@spheremall.com)
+ * Date: 17.04.2018
+ * Time: 14:39
+ */
+
+namespace SphereMall\MS\Entities;
+
+/**
+ * Class Brand
+ * @package SphereMall\MS\Entities
+ * @property int $id
+ * @property string $title
+ * @property string $code
+ * @property int $visible
+ * @property string $group
+ * @property int $coefficient
+ */
+
+class UnitOfMeasure
+{
+    #region [Properties]
+    public $id;
+    public $title;
+    public $code;
+    public $visible;
+    public $group;
+    public $coefficient;
+    #endregion
+}
