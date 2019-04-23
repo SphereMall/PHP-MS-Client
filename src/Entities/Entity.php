bcb72f809af0133a236b568c49a48e745a4f737a
sauron918 <sauron918@gmail.com>
2019-04-23 09:44:11 +0300

    Implement JsonSerializable for Entity

--- src/Entities/Entity.php
+++ src/Entities/Entity.php
@@ -9,6 +9,7 @@
 
 namespace SphereMall\MS\Entities;
 
+use JsonSerializable;
 use SphereMall\MS\Lib\BaseModel;
 use SphereMall\MS\Lib\Helpers\ClassReflectionHelper;
 
@@ -16,7 +17,7 @@ use SphereMall\MS\Lib\Helpers\ClassReflectionHelper;
  * Class Entity
  * @package SphereMall\MS\Entities
  */
-class Entity extends BaseModel
+class Entity extends BaseModel implements JsonSerializable
 {
     #region [Public methods]
 
@@ -26,6 +27,23 @@ class Entity extends BaseModel
     public function getType()
     {
         return (new ClassReflectionHelper(get_called_class()))->getShortLowerCaseName();
+    }
+
+    /**
+     * Specify data which should be serialized to JSON.
+     * Includes all public properties by default plus data from properties array.
+     *
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        $publicProperties = get_object_vars($this);
+        unset($publicProperties['properties']);
+
+        return array_merge($publicProperties, $this->getPropertiesField());
     }
     #endregion
 }
