d2702622f6daf2fcf067c0ea12a62961f71cd5a9
D.Matviychuk <d.matviychuk@spheremall.com>
2019-05-17 16:08:48 +0300

    add entities ids and change version to 2.8.4.9

--- src/Lib/Constants/Entities.php
+++ src/Lib/Constants/Entities.php
@@ -14,12 +14,24 @@ namespace SphereMall\MS\Lib\Constants;
  */
 class Entities
 {
-    const PRODUCT      = 1;
-    const BRAND        = 2;
-    const USER         = 3;
+    const PRODUCT = 1;
+    const BRAND = 2;
+    const USER = 3;
     const CATALOG_ITEM = 4;
     const FACTOR_VALUE = 5;
-    const PROMOTION    = 6;
-    const FACTOR       = 7;
-    const DOCUMENT     = 8;
+    const PROMOTION = 6;
+    const FACTOR = 7;
+    const DOCUMENT = 8;
+    const ATTRIBUTE_VALUES = 9;
+    const ATTRIBUTES = 10;
+    const DEALERS = 11;
+    const CATALOG = 12;
+    const PAGES = 13;
+    const STATIC_TEXTS = 14;
+    const MASTER_PAGES = 15;
+    const PRODUCT_GROUPS = 16;
+    const FUNCTIONAL_NAMES = 17;
+    const CATEGORIES = 18;
+    const ENTITY_GROUPS = 19;
+    const MENU_ITEMS = 20;
 }
