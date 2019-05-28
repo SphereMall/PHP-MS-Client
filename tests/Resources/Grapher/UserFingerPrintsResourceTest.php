2a5cb6f4e091a019c1302ad3a67728e2312bd6fa
Dmitriy Vorobey <dmitriy@spheremall.com>
2019-05-28 15:41:50 +0300

    UserFingerPrint mapper fixed

--- tests/Resources/Grapher/UserFingerPrintsResourceTest.php
+++ tests/Resources/Grapher/UserFingerPrintsResourceTest.php
@@ -3,7 +3,7 @@
 namespace SphereMall\MS\Tests\Resources\Grapher;
 
 use GuzzleHttp\Exception\GuzzleException;
-use SphereMall\MS\Entities\UserFingerprints;
+use SphereMall\MS\Entities\UserFingerPrints;
 use SphereMall\MS\Tests\Resources\SetUpResourceTest;
 
 /**
@@ -11,13 +11,13 @@ use SphereMall\MS\Tests\Resources\SetUpResourceTest;
  *
  * @package SphereMall\MS\Tests\Resources\Grapher
  */
-class UserFingerprintsResourceTest extends SetUpResourceTest
+class UserFingerPrintsResourceTest extends SetUpResourceTest
 {
     #region [Test methods]
     public function testCRUD()
     {
         $this->assertNotEmpty($this->client->userFingerprints()->all());
-        $this->assertInstanceOf(UserFingerprints::class, $this->client->userFingerprints()->get(1));
+        $this->assertInstanceOf(UserFingerPrints::class, $this->client->userFingerprints()->get(1));
     }
 
     /**
@@ -27,7 +27,7 @@ class UserFingerprintsResourceTest extends SetUpResourceTest
     {
         $result = $this->client->userUidHash()->getByUser(1);
         foreach ($result as $item) {
-            $this->assertInstanceOf(UserFingerprints::class, $item);
+            $this->assertInstanceOf(UserFingerPrints::class, $item);
         }
     }
     #endregion
