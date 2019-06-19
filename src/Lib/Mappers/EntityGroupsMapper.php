1a263ebe70769c45c6e4a68c63cf759e60138f50
Sergey Kondratovec <skondratovec@Sergeys-MacBook-Pro.local>
2019-06-19 17:13:02 +0300

    Fix media mappers

--- src/Lib/Mappers/EntityGroupsMapper.php
+++ src/Lib/Mappers/EntityGroupsMapper.php
@@ -50,17 +50,20 @@ class EntityGroupsMapper extends Mapper
         if (isset($this->data['mediaEntities'])) {
             foreach ($this->data['mediaEntities'] ?? [] as $mediaEntity) {
 
-                if (isset($mediaEntity['relationships']['media'][0]['attributes'])) {
-                    $mediaData = array_merge($mediaEntity['relationships']['media'][0]['attributes'], $mediaEntity['attributes']);
-                } else {
-                    $mediaData = array_merge($this->data['media'][$mediaEntity['mediaId']], $mediaEntity);
+                if (isset($mediaEntity['media'][0])) {
+
+                    $media = new Media($mediaEntity['media'][0]);
+                    if (!$this->entityGroup->mainMedia) {
+                        $this->entityGroup->mainMedia = $media;
+                    }
+                    $result[$mediaEntity['id']] = $media;
                 }
-                $media = new Media($mediaData);
-                if (!$this->entityGroup->mainMedia) {
-                    $this->entityGroup->mainMedia = $media;
-                }
-                $result[$mediaEntity['attributes']['mediaId'] ?? $mediaEntity['mediaId']] = $media;
             }
+
+            $this->entityGroup->media = $result;
+
+            return $this;
+
         }
 
         $this->entityGroup->media = $result;
--- src/Lib/Mappers/ProductsMapper.php
+++ src/Lib/Mappers/ProductsMapper.php
@@ -174,18 +174,23 @@ class ProductsMapper extends Mapper
         if (isset($this->data['mediaEntities'])) {
             foreach ($this->data['mediaEntities'] ?? [] as $mediaEntity) {
 
-                if(isset($mediaEntity['relationships']['media'][0]['attributes'])){
-                    $mediaData = array_merge($mediaEntity['relationships']['media'][0]['attributes'], $mediaEntity['attributes']);
-                } else{
-                    $mediaData = array_merge($this->data['media'][$mediaEntity['mediaId']], $mediaEntity);
+                if (isset($mediaEntity['media'][0])) {
+
+                    $media = new Media($mediaEntity['media'][0]);
+                    if (!$this->product->mainMedia) {
+                        $this->product->mainMedia = $media;
+                    }
+                    $result[$mediaEntity['id']] = $media;
                 }
-                $media = new Media($mediaData);
-                if (!$this->product->mainMedia) {
-                    $this->product->mainMedia = $media;
-                }
-                $result[$mediaEntity['attributes']['mediaId'] ?? $mediaEntity['mediaId']] = $media;
             }
-        } elseif (isset($this->data['media'])) {  // old structure
+
+            $this->product->media = $result;
+
+            return $this;
+
+        }
+
+        if (isset($this->data['media'])) {  // old structure
             $mapper = new ImagesMapper();
             foreach ($this->data['media'] as $image) {
                 $result[] = $mapper->createObject($image);
